9f74cef35a50c2668ac5ba7e4e624f56da1e4ad7
MarJuice <mariushanss1@gmail.com>
2025-03-13 18:08:18 +0100

    Create fetch post functionality and solar system API. Compare helper function for first task. Write data to json file to solve task easier

--- app.mjs
+++ app.mjs
@@ -7,3 +7,44 @@ const startURL = `${RIS}start?player=${player}`;
 const start = await fetch(startURL, { method: 'GET' });
 const startResponse = await start.json();
 console.log(startResponse);
+
+const answerURL = `${RIS}answer`;
+async function answer(solution) {
+      
+    const request = await fetch(answerURL, {
+        method: 'POST',
+        body: JSON.stringify({ answer: solution, player: player}),
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+
+    const response = await request.json();
+    console.log(response);
+
+}
+
+// Find difference between the equatorial radius and the mean radius of the Sun
+let data = await corpAPI("sun")
+    .then(data => compare(data.meanRadius, data.equaRadius));
+    console.log(data);
+answer(data);
+
+async function compare(value1, value2) {
+    let diff = value1 - value2;
+    return Math.abs(diff);
+}
+
+async function corpAPI(query) {
+
+    const request = await fetch(API+query,
+        { 
+            method: 'GET', 
+            headers: { 'Content-Type': 'application/json' } 
+        }
+    );
+
+    const data = await request.json();
+    writeFile('data.json', JSON.stringify(data, null, 2));
+    return data;
+}
